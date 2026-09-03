d5da3e04d3795e73b498e4e1e2c099e5cbe43475
Ryan <ryanwbadenhorst@gmail.com>
2026-09-03 17:22:58 +0200

    Initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,27 @@
+import type { CSSProperties } from "react";
+import { NavLink } from "react-router-dom";
+
+export default function Header() {
+    const activeStyles: CSSProperties = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616",
+        backgroundColor: "#bebebe"
+    }
+
+    return (
+        <div className="navbar">
+            <nav>
+                <NavLink to="/" style={({ isActive }) => isActive ? activeStyles : undefined}> Home </NavLink>
+
+                <NavLink to="/products" style={({ isActive }) => isActive ? activeStyles : undefined}> Products </NavLink>
+
+                <NavLink to="/admin" style={({ isActive }) => isActive ? activeStyles : undefined}> Cart </NavLink>
+
+                <NavLink to="/login" style={({ isActive }) => isActive ? activeStyles : undefined}> Wishlist </NavLink>
+
+                <NavLink to="/profile" style={({ isActive }) => isActive ? activeStyles : undefined}> Profile </NavLink>
+            </nav>
+        </div>
+    )
+}
