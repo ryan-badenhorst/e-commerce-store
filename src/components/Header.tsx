e5280682e4901df65eb8e56e581d30e1d43bd268
Ryan <ryanwbadenhorst@gmail.com>
2026-09-04 17:54:05 +0200

    Updated Navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,9 +17,9 @@ export default function Header() {
                 <NavLink to="/products" style={({ isActive }) => isActive ? activeStyles : undefined}> Products </NavLink>
 
                 <div className="left-nav">
-                    <NavLink to="/admin" style={({ isActive }) => isActive ? activeStyles : undefined}> Cart </NavLink>
-
                     <NavLink to="/login" style={({ isActive }) => isActive ? activeStyles : undefined}> Wishlist </NavLink>
+
+                    <NavLink to="/admin" style={({ isActive }) => isActive ? activeStyles : undefined}> Cart </NavLink>
 
                     <NavLink to="/profile" style={({ isActive }) => isActive ? activeStyles : undefined}> Profile </NavLink>
                 </div>
