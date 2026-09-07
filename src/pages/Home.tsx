43c91aa9467913050b684e7f41697d474d6efe44
Ryan <ryanwbadenhorst@gmail.com>
2026-09-07 13:45:39 +0200

    Updated Homepage styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import homeHero from "../images/home-hero-five.jpg"
+import homeHero from "../images/home-hero.jpg"
 
 export default function Home() {
     return (
@@ -11,13 +11,11 @@ export default function Home() {
                 />
 
                 <div className="absolute inset-0 flex items-center">
-                    <div className="ml-36">
-                        <h1 className="text-4xl font-bold text-white">
-                            Elevate the Everyday
-                            <br />
-                            Modern pieces. Effortless style.
-                            <br />
-                            Discover jewellery and accessories designed to add something extra to the everyday.
+                    <div className="ml-25">
+                        <h1 className="text-4xl font-serif font-bold text-amber-700 flex flex-col">
+                            <span className="mb-4">Elevate the Everyday</span>
+                            
+                            Jewellery and accessories <span className="new-line">designed to elevate the everyday</span>
                         </h1>
                     </div>
                 </div>
