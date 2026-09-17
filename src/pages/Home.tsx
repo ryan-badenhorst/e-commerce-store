720c782ff09cf778ecebad6c232fe3155bd5a6ee
Ryan <ryanwbadenhorst@gmail.com>
2026-09-17 14:05:59 +0200

    Updated Homepage styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom"
 
+import { FaArrowRightLong } from "react-icons/fa6";
+
 import homeHero from "../images/home-hero.jpg"
 import mensHome from "../images/mens-home.jpg"
 import womensHome from "../images/womens-home.jpg"
+import unisexHome from "../images/unisex-home.jpg"
 
 export default function Home() {
     return (
@@ -16,7 +19,7 @@ export default function Home() {
 
                 <div className="absolute inset-0 flex items-center">
                     <div className="ml-25">
-                        <h1 className="text-4xl font-serif font-bold text-amber-700 flex flex-col">
+                        <h1 className="text-4xl font-serif font-bold text-orange-400 flex flex-col">
                             <span className="mb-4">Elevate the Everyday</span>
                             
                             Jewellery and accessories <span className="new-line">designed to elevate the everyday</span>
@@ -25,9 +28,9 @@ export default function Home() {
                 </div>
             </div>
 
-            <div className="mx-25 my-10">
+            <div className="mx-25 my-7">
                 
-                <h1 className="font-serif text-center text-4xl mb-10">Explore Our Collections</h1>
+                <h1 className="font-serif text-center text-4xl mb-7">Explore Our Collections</h1>
 
                 <div className="flex flex-row gap-5 justify-between">
                     <Link to="/products?gender=men" className="home-link">
@@ -35,7 +38,15 @@ export default function Home() {
 
                         <div className="home-overlay"></div>
 
-                        <span>For men</span>
+                        <span>For men <FaArrowRightLong /></span>
+                    </Link>
+
+                    <Link to="/products?gender=unisex" className="home-link">
+                        <img src={unisexHome} alt="Unisex jewelry" />
+
+                        <div className="home-overlay"></div>
+
+                        <span>For everyone <FaArrowRightLong /></span>
                     </Link>
 
                     <Link to="/products?gender=women" className="home-link">
@@ -43,15 +54,7 @@ export default function Home() {
 
                         <div className="home-overlay"></div>
 
-                        <span>For women</span>
-                    </Link>
-
-                    <Link to="/products?gender=unisex" className="home-link">
-                        <img src={womensHome} alt="Unisex jewelry" />
-
-                        <div className="home-overlay"></div>
-
-                        <span>For everyone</span>
+                        <span>For women <FaArrowRightLong /></span>
                     </Link>
                 </div>
             </div>
